0cae12bf71214d316d211693b77c6693ced8545c
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-07-27 23:06:04 +0100

    BaseTrackerClusterizer: Fix compilation errors

--- source/falaise/snemo/processing/base_tracker_clusterizer.cc
+++ source/falaise/snemo/processing/base_tracker_clusterizer.cc
@@ -99,8 +99,8 @@ void base_tracker_clusterizer::_initialize(const datatools::properties &setup_) 
 
   // Get the Geiger cell locator from geometry plugins :
   auto locator_plugin_name = ps.get<std::string>("locator_plugin_name","");
-  auto& snLocator = snemo::geometry::getSNemoLocator(get_geometry_manager(), locator_plugin_name);
-  geigerLocator_ = &(snLocator.get_gg_locator());
+  auto snLocator = snemo::geometry::getSNemoLocator(get_geometry_manager(), locator_plugin_name);
+  geigerLocator_ = &(snLocator->get_gg_locator());
 
   // Cell geom_id mask
   auto cell_id_mask_rules = ps.get<std::string>("cell_id_mask_rules","");
