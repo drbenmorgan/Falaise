3155771a5430f86cf6a9539a4a6a0984753c87c7
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-07-27 23:07:12 +0100

    BaseTrackerFitter: Fix compilation errors

--- source/falaise/snemo/processing/base_tracker_fitter.cc
+++ source/falaise/snemo/processing/base_tracker_fitter.cc
@@ -91,8 +91,8 @@ void base_tracker_fitter::_initialize(const datatools::properties& setup_) {
   _maximum_number_of_fits_ = ps.get<int>("maximum_number_of_fits", 0);
 
   auto locator_plugin_name = ps.get<std::string>("locator_plugin_name","");
-  auto& snLocator = snemo::geometry::getSNemoLocator(get_geometry_manager(),locator_plugin_name);
-  geigerLocator_ = &(snLocator.get_gg_locator());
+  auto snLocator = snemo::geometry::getSNemoLocator(get_geometry_manager(),locator_plugin_name);
+  geigerLocator_ = &(snLocator->get_gg_locator());
   DT_THROW_IF(geigerLocator_ == nullptr, std::logic_error, "Cannot find Geiger locator !");
 }
 
